e57d228cdbc3253e093ba5565be1d1838cb040d4
Zack Sloan <ZackSloanWorks@gmail.com>
2020-12-17 16:36:43 -0600

    First commit

---
+++ Studio2.6AreaOfACircle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Studio2._6AreaOfACircle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
